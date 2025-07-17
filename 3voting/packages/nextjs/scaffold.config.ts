0482470ffe709b9f1e7009fb88ac9dd84e483a58
exo404 <albertopetillo@hotmail.it>
2025-07-17 03:18:18 +0200

    refactor: config

--- 3voting/packages/nextjs/scaffold.config.ts
+++ 3voting/packages/nextjs/scaffold.config.ts
@@ -15,7 +15,7 @@ export const DEFAULT_ALCHEMY_API_KEY = "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
 
 import { defineChain } from 'viem';
 
-const 3votingchain = defineChain({
+const votingchain = defineChain({
   id: 1771,
   name: '3votingchain',
   network: '3votingchain-local',
@@ -36,7 +36,7 @@ const 3votingchain = defineChain({
 
 const scaffoldConfig = {
   // The networks on which your DApp is live
-  targetNetworks: [chains.hardhat],
+  targetNetworks: [votingchain],
   // The interval at which your front-end polls the RPC servers for new data (it has no effect if you only target the local network (default is 4000))
   pollingInterval: 30000,
   // This is ours Alchemy's default API key.
